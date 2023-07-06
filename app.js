f19f1a665fd17a6c2af52290e3bb1726538163c9
yingtu35 <91551415+yingtu35@users.noreply.github.com>
2023-07-06 16:49:48 +0800

    Purposefully fail the deployment

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('4')
+})
+
 app.get('/health', (req, res) => {
+  throw 'error'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
